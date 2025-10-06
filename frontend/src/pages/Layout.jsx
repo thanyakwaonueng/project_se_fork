78610d6816e4288cd8a3e1ec1cce5b0524577803
thanyakwaonueng <kwaonueng@gmail.com>
2025-10-06 15:14:40 +0700

    fuck you again

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { AuthProvider } from '../util_lib/auth';
+import { AuthProvider } from '../lib/auth';
 
 export default function Layout() {
   const location = useLocation();  // ดึง path ปัจจุบัน
